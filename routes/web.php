0a0ca012c82f64566db1a089acbe82615a483bbe
selvpole <ychsiao@tlaradock.ychsiao>
2019-09-26 02:29:23 -0400

    🐈 Update Route List

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/todo', 'TodoController')->except([
-    'show'
-]);
+Route::resource('/todo', 'TodoController')->except([ 'show' ]);
